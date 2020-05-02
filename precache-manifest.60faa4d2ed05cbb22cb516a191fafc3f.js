8d2a7b9a18ca47a14416f77bb11801787d872b9d
Yunsoo Kim <yunsoo@umich.edu>
2020-05-02 15:39:04 -0400

    Updates

---
+++ precache-manifest.60faa4d2ed05cbb22cb516a191fafc3f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0ca5e9a8ca2a1cbcaa7afd2eeeb19a8",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3819aec4112f7505c2e2",
+    "url": "/portfolio/static/css/main.9ffb5442.chunk.css"
+  },
+  {
+    "revision": "63a11fd8a77d8dfb2154",
+    "url": "/portfolio/static/js/2.905aae00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.905aae00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3819aec4112f7505c2e2",
+    "url": "/portfolio/static/js/main.63bb47fa.chunk.js"
+  },
+  {
+    "revision": "822e9c2bcb7c72f41cee",
+    "url": "/portfolio/static/js/runtime-main.46b98133.js"
+  },
+  {
+    "revision": "54884c22d84c572f0e57cb367259f3ff",
+    "url": "/portfolio/static/media/Modeka.54884c22.otf"
+  },
+  {
+    "revision": "79b718677a18b48b53927d966b6b6413",
+    "url": "/portfolio/static/media/TAMWORTH.79b71867.TTF"
+  },
+  {
+    "revision": "525c0b0f90e76a4f75c1281859d2ea82",
+    "url": "/portfolio/static/media/Yunsoo_Resume.525c0b0f.pdf"
+  },
+  {
+    "revision": "e16ee888ef183c5dc99a9da45d2fac7c",
+    "url": "/portfolio/static/media/email_icon.e16ee888.svg"
+  },
+  {
+    "revision": "9329315b1b1acb1ab416b8dbb86ce46b",
+    "url": "/portfolio/static/media/github_icon.9329315b.svg"
+  },
+  {
+    "revision": "14f7b6753cb58692c0da571c41b33210",
+    "url": "/portfolio/static/media/linkedin_icon.14f7b675.svg"
+  },
+  {
+    "revision": "c13e65cc1ab0997bb98f2167bfac07e8",
+    "url": "/portfolio/static/media/menu_icon.c13e65cc.svg"
+  },
+  {
+    "revision": "585bfb2babb8bbaa73e8c66d73bffd65",
+    "url": "/portfolio/static/media/poly.585bfb2b.png"
+  },
+  {
+    "revision": "c1fe84da915cd27593401156572959b8",
+    "url": "/portfolio/static/media/resume_icon.c1fe84da.svg"
+  }
+]);
